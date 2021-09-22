b8749d000ae58ec1326632ce815e6bc7bc92b651
pakornyodkab <yodpakorn2543@gmail.com>
2021-09-22 17:26:09 +0700

    do main page and create new post already

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :users
   get 'user/delview/:id',to:"users#delview", as: 'user_delview'
   get 'user/create_fast/:name/:email' ,to:"users#create_fast" ,as: 'user_create_fast'
   get'/main' ,to:"users#main"
   post'/main' ,to:"users#findbyemail"
+  get '/newpostbyuser/:id' ,to:"posts#newpostbyuser" ,as: 'newpostbyuser'
+  post '/newpostbyuser/:id' ,to:"posts#createbyuser"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_072249) do
+ActiveRecord::Schema.define(version: 2021_09_22_083054) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "msg"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_09_16_072249) do
     t.string "postal_code"
   end
 
+  add_foreign_key "posts", "users"
 end
